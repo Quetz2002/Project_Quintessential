a549669cde1710fe38d85c0f90bd15f743698e1d
GiovanniLaguna <erick.laguna@saei.mx>
2026-01-30 00:25:15 -0600

    Interactible

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,11 +14,36 @@ public class PlayerMovement : NetworkBehaviour
         Vector3 move = new Vector3(x, 0, z) * speed * Time.deltaTime;
 
         MoveServerRPC(move);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryToCollect();
+        }
     
     }
     [ServerRpc]
     void MoveServerRPC(Vector3 move)
     {
         transform.Translate(move);
+
+    }
+
+    void TryToCollect()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, 1.5f);
+
+        foreach (Collider hit in hits)
+        {
+            CollectibleItem collectible = hit.GetComponent<CollectibleItem>();
+            if (collectible != null)
+            {
+                collectible.CollectServerRpc();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 1f);
     }
 }
