5d69f37f24d16d771fdab1f01246b5a73f53067f
Quetz2002 <quetz.yggdrasil24@outlook.com>
2026-02-05 22:56:41 -0600

    Create PlayerMovement.cs

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float speed = 25f;
+
+    private void Update()
+    {
+        if (!IsOwner) return;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0, z) * speed * Time.deltaTime;
+
+        MoveServerRPC(move);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryToCollect();
+        }
+    
+    }
+    [ServerRpc]
+    void MoveServerRPC(Vector3 move)
+    {
+        transform.Translate(move);
+
+    }
+
+    void TryToCollect()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, 1.5f);
+
+        foreach (Collider hit in hits)
+        {
+            CollectibleItem collectible = hit.GetComponent<CollectibleItem>();
+            if (collectible != null)
+            {
+                collectible.CollectServerRpc();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 1f);
+    }
+}
